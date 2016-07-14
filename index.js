f7304a328dc06b7564acdfca39622ea367265c80
Greg Yotz <greg@yotzee.com>
2016-07-14 15:13:11 -0500

    removed old code

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
 "use strict";
 
 module.exports = require('./lib/discover');
-
-var discoveryClient = require('khs-discover');
-discoveryClient.config({
-    client: {
-        hostname: "localhost",
-        port: 8762,
-        name: "test app"
-    }
-});
-discoveryClient.EnableDiscoveryClient();
-
-
-var discoveryServer = require('khs-discover');
-discoveryServer.config({
-    server: {
-        name: "ksh-discover",
-        port: 8762
-    }
-});
-discoveryServer.EnableDiscoveryServer();
